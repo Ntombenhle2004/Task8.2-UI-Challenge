9b2048fff56277e1223088cb661d7c09a0c398d9
ntorh <ngcobontombenhle434@gmail.com>
2025-09-03 17:05:27 +0200

    created task 2.2 using react

---
+++ vite-Task8-1-UI-Challenge/src/components/email.tsx
@@ -0,0 +1,17 @@
+import errorIcon from "../assets/images/icon-error.svg";
+import Arrow from "../assets/images/icon-arrow.svg";
+
+export default function Email() {
+  return (
+    <>
+      <div className="Email">
+        <div className="email">
+          <p className="gmail">EmailAddress</p>
+          <img src={errorIcon} alt="errorIcon" className="errorIcon"></img>
+          <img src={Arrow} alt="Arrow" className="Arrow"></img>
+        </div>
+      </div>
+      <p className="errorMsg">Please provide a valid email</p>
+    </>
+  );
+}
---
+++ vite-Task8-1-UI-Challenge/src/components/heading.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Heading() {
+  return (
+    <div>
+      <div className="myheadings">
+        <h1 className="heading1">WE'RE</h1>
+        <h1 className="heading2">
+          COMING <br />
+          SOON
+        </h1>
+      </div>
+    </div>
+  );
+}
