43d8bb440da53153302aa0bc7e0790679ab278d6
ntorh <ngcobontombenhle434@gmail.com>
2025-09-11 14:45:47 +0200

    i tried to make the emage to look good i tried to make it a background image but it refusing

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,20 @@ import Logo from "./components/logo";
 import Heading from "./components/heading";
 import Paragraph from "./components/paragraph";
 import Email from "./components/email";
-import Row2Img from "./components/row2Img";
+// import Row2Img from "./components/row2Img";
 
 function App() {
   return (
     <>
-      <Logo />
       <div className="mySection">
         <div className="row1">
+          <Logo />
           <Heading />
           <Paragraph />
           <Email />
         </div>
 
-        <div className="row2">
-          <Row2Img />
-        </div>
+        <div className="row2">{/* <Row2Img /> */}</div>
       </div>
     </>
   );
--- src/components/row2Img.tsx
+++ src/components/row2Img.tsx
@@ -1,11 +1,11 @@
-import lastImg from "../assets/images/hero-desktop.jpg";
+// import lastImg from "../assets/images/hero-desktop.jpg";
 
-export default function Row2Img() {
-  return (
-    <div>
-      <div className="row2">
-        <img src={lastImg} alt="lastImg" className="lastImg"></img>
-      </div>
-    </div>
-  );
-}
+// export default function Row2Img() {
+//   return (
+//     <div>
+//       <div className="row2">
+//         <img src={lastImg} alt="lastImg" className="lastImg"></img>
+//       </div>
+//     </div>
+//   );
+// }
